e46a66ddc9e02fb04b5ea9d9394173c64a2b09f7
mennek <46343675+mennek@users.noreply.github.com>
2019-01-03 13:23:46 +0100

    Update Geolocation.java

--- src/android/Geolocation.java
+++ src/android/Geolocation.java
@@ -15,12 +15,30 @@
        under the License.
  */
 
-
+package com.google.android.gms.location.sample.backgroundlocationupdates;
 package org.apache.cordova.geolocation;
 
 import android.content.pm.PackageManager;
 import android.Manifest;
 import android.os.Build;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.preference.PreferenceManager;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.Snackbar;
+import android.widget.Button;
+import android.widget.TextView;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaArgs;
@@ -30,6 +48,11 @@ import org.apache.cordova.PluginResult;
 import org.apache.cordova.LOG;
 import org.json.JSONArray;
 import org.json.JSONException;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
 
 import javax.security.auth.callback.Callback;
 
